6d9dc9c5b0a172f7f0d26fc3c7ee994397d9a71a
jsmadja <julien.smadja@gmail.com>
2011-11-01 09:25:15 +0100

    add all clearer strategy

---
+++ sql/neogeo-hiscores-1.3.2.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `hiscores`.`TITLE` (`ID`, `DESCRIPTION`, `CLASSNAME`, `LABEL`, `POSITION`) VALUES (10, 'Finish a game with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearerTitleStrategy', 'All clearer!', 2000);
+
