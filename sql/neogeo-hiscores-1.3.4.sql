ce4417a0180d553515c51ac9c8a802d44126f8d0
jsmadja <julien.smadja@gmail.com>
2011-11-04 20:46:17 +0100

    add Title Strategy All clear 2, 5, 10 sql

--- sql/neogeo-hiscores-1.3.4.sql
+++ sql/neogeo-hiscores-1.3.4.sql
@@ -1,3 +1,3 @@
-INSERT INTO TITLE (`DESCRIPTION`, `CLASSNAME`, `LABEL`, `POSITION`) VALUES ('Finish 2 games with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearer2TitleStrategy', 'All clearer! x2', 2002);
-INSERT INTO TITLE (`DESCRIPTION`, `CLASSNAME`, `LABEL`, `POSITION`) VALUES ('Finish 5 games with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearer5TitleStrategy', 'All clearer! x5', 2005);
-INSERT INTO TITLE (`DESCRIPTION`, `CLASSNAME`, `LABEL`, `POSITION`) VALUES ('Finish 10 games with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearer10TitleStrategy', 'All clearer! x10', 2010);
+INSERT INTO TITLE VALUES (11, 'Finish 2 games with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearer2TitleStrategy', 'All clearer! x2', 2002);
+INSERT INTO TITLE VALUES (12, 'Finish 5 games with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearer5TitleStrategy', 'All clearer! x5', 2005);
+INSERT INTO TITLE VALUES (13, 'Finish 10 games with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearer10TitleStrategy', 'All clearer! x10', 2010);
