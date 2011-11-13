3e558ebfe23d68b2f730d0a8f0b24792b88a7b66
jsmadja <julien.smadja@gmail.com>
2011-11-13 14:59:35 +0100

    add license header

--- sql/neogeo-hiscores-1.3.2.sql
+++ sql/neogeo-hiscores-1.3.2.sql
@@ -1,2 +1,18 @@
+--
+--     Copyright (C) 2011 Julien SMADJA <julien dot smadja at gmail dot com>
+--
+--     Licensed under the Apache License, Version 2.0 (the "License");
+--     you may not use this file except in compliance with the License.
+--     You may obtain a copy of the License at
+--
+--             http://www.apache.org/licenses/LICENSE-2.0
+--
+--     Unless required by applicable law or agreed to in writing, software
+--     distributed under the License is distributed on an "AS IS" BASIS,
+--     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--     See the License for the specific language governing permissions and
+--     limitations under the License.
+--
+
 INSERT INTO `hiscores`.`TITLE` (`ID`, `DESCRIPTION`, `CLASSNAME`, `LABEL`, `POSITION`) VALUES (10, 'Finish a game with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearerTitleStrategy', 'All clearer!', 2000);
 
--- sql/neogeo-hiscores-1.3.4.sql
+++ sql/neogeo-hiscores-1.3.4.sql
@@ -1,3 +1,19 @@
+--
+--     Copyright (C) 2011 Julien SMADJA <julien dot smadja at gmail dot com>
+--
+--     Licensed under the Apache License, Version 2.0 (the "License");
+--     you may not use this file except in compliance with the License.
+--     You may obtain a copy of the License at
+--
+--             http://www.apache.org/licenses/LICENSE-2.0
+--
+--     Unless required by applicable law or agreed to in writing, software
+--     distributed under the License is distributed on an "AS IS" BASIS,
+--     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--     See the License for the specific language governing permissions and
+--     limitations under the License.
+--
+
 INSERT INTO TITLE VALUES (11, 'Finish 2 games with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearer2TitleStrategy', 'All clearer! x2', 2002);
 INSERT INTO TITLE VALUES (12, 'Finish 5 games with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearer5TitleStrategy', 'All clearer! x5', 2005);
 INSERT INTO TITLE VALUES (13, 'Finish 10 games with only one credit', 'com.anzymus.neogeo.hiscores.success.AllClearer10TitleStrategy', 'All clearer! x10', 2010);
