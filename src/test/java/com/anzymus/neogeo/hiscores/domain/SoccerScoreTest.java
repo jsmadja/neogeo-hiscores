d3d547430d87234b50aa881f924115ea11371101
jsmadja <julien.smadja@gmail.com>
2012-02-27 11:42:39 +0100

    add soccer score test

--- src/test/java/com/anzymus/neogeo/hiscores/domain/SoccerScoreTest.java
+++ src/test/java/com/anzymus/neogeo/hiscores/domain/SoccerScoreTest.java
@@ -17,17 +17,49 @@
 package com.anzymus.neogeo.hiscores.domain;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class SoccerScoreTest {
 
-    @Test
-    public void should_compare_two_equal_score() {
-        Score score = new Score("2-3-4");
-        SoccerScore score1 = new SoccerScore(score);
-        SoccerScore score2 = new SoccerScore(score);
+	// score board example
+	// 7 7 11 3 8
+	// 7 6 13 1 12
+	// 7 6 8 5 3
+	// 6 6 15 8 7
+	// 6 5 10 11 -1
+	// 5 4 10 6 4
+	// 5 4 9 7 2
+	// 5 4 8 6 2
+	// 5 4 10 9 1
+	// 4 3 8 6 2
 
-        assertEquals(0, score1.compareTo(score2));
-    }
+	@Test
+	public void t() {
+		SoccerScore score1 = new SoccerScore(new Score("2-3-4"));
+		SoccerScore score2 = new SoccerScore(new Score("2-3-5"));
+		assertEquals(1, score1.compareTo(score2));
+	}
 
+	@Test
+	public void should_compare_two_equal_score() {
+		Score score = new Score("2-3-4");
+		SoccerScore score1 = new SoccerScore(score);
+		SoccerScore score2 = new SoccerScore(score);
+		assertEquals(0, score1.compareTo(score2));
+	}
+
+	@Test
+	public void should_compare_two_different_scores() {
+		SoccerScore score1 = new SoccerScore(new Score("2-3-4"));
+		SoccerScore score2 = new SoccerScore(new Score("2-3-5"));
+		assertEquals(1, score1.compareTo(score2));
+	}
+
+	@Test
+	public void should_compare_two_different_scores_reverse() {
+		SoccerScore score1 = new SoccerScore(new Score("2-3-5"));
+		SoccerScore score2 = new SoccerScore(new Score("2-3-4"));
+		assertEquals(-1, score1.compareTo(score2));
+	}
 }
